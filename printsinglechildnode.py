625b712235f0815ed03feb7cc9f9f318bc7d7493
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-07-05 13:17:20 +0530

    Add files via upload

---
+++ printsinglechildnode.py
@@ -0,0 +1,90 @@
+class btree():
+  def __init__(self, data, right=None, left=None):
+    self.data = data
+    self.right = right
+    self.left = left
+   
+  
+
+class pair():
+  def __init__(self, node, state):
+    self.state = state
+    self.node = node
+
+m = int(input())
+n = list(map(str, input().split()))
+
+
+def btreeform(n, s, root):
+  
+  root = btree(int(n[0]))
+  
+  
+  p = pair(root,1)
+  s.append(p)
+  idx =0
+  while(len(s)>0):
+    top = s[-1]
+    if(top.state == 1):
+    
+      idx+=1
+      if(n[idx]!="n"):
+        t = btree(int(n[idx])) 
+        top.node.left = t
+      
+        p = pair(t, 1)
+        s.append(p)
+      else:
+        top.node.left = None
+      
+      top.state += 1
+    
+    
+    elif(top.state == 2):
+      idx+=1
+      if(n[idx]!="n"):
+        t = btree(int(n[idx]) ,None, None)
+        top.node.right = t
+      
+        p = pair(t, 1)
+        s.append(p)
+      else:
+        top.node.right = None
+      
+      top.state += 1
+     
+    else:
+      s.pop()
+
+
+  return root
+  
+  
+root = btreeform(n, [], None)
+
+
+def singlecn(root, proot):
+  
+  
+  if(root == None):
+    if(proot.left!=None):
+      print(proot.left.data)  
+    else:
+      print(proot.right.data)
+    return
+  
+  if(root.left ==None and root.right ==None):
+    return None
+
+  
+  singlecn(root.left, root)
+ 
+  singlecn(root.right, root)
+  
+  
+  return 
+  
+
+
+singlecn(root, None)
+ 
